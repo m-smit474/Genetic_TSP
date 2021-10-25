91ff145524dd1c3b26c52723d07bd60c6b51c6c7
Matt Smith <smithmw99@gmail.com>
2021-10-24 23:51:45 -0600

    distance function

--- test.py
+++ test.py
@@ -3,3 +3,15 @@ import pandas as pd
 tsp = pd.read_csv("Data/tsp1.csv", index_col=0)
 
 print(tsp)
+
+def pointDistance(x1, y1, x2, y2):
+    distance = ((x2-x1)**2 + (y2-y1)**2)**0.5
+    return distance
+
+
+# Loops though each city (row) 
+# Can use distance function to find minimum distance between each city
+for i in range(0, tsp.shape[0]):
+    city = tsp.iloc[i]
+
+    print(city)
