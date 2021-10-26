079732f2bf6d0b6db6e5ec3117b0fd6bedd3ef8f
Matt Smith <smithmw99@gmail.com>
2021-10-25 21:20:55 -0600

    Fitness function

--- test.py
+++ test.py
@@ -1,17 +1,36 @@
 import pandas as pd
+import random
 
 tsp = pd.read_csv("Data/tsp1.csv", index_col=0)
 
-print(tsp)
+# gets number of rows
+numRows = tsp.shape[0]
 
+# Calculates distance between two coordinates
 def pointDistance(x1, y1, x2, y2):
     distance = ((x2-x1)**2 + (y2-y1)**2)**0.5
     return distance
 
+# Generates a random solution
+def solution():
+    sol = []
+    for i in range(numRows):
+        sol.append(random.randint(1,numRows))
+    return sol
 
-# Loops though each city (row) 
-# Can use distance function to find minimum distance between each city
-for i in range(0, tsp.shape[0]):
-    city = tsp.iloc[i]
+# Determines total distance of a given solution
+# ** I believe this currently doesn't count the last city
+def fitness(solution):
+    totalDistance = 0
+    for i in range(len(solution) - 1):
+        city = tsp.iloc[i]
+        city2 = tsp.iloc[i+1]
+        totalDistance += pointDistance(city.loc["x"],city.loc["y"],city2.loc["x"],city2.loc["y"])
 
-    print(city)
+# Return inverse of distance
+# Small distance is high fitness
+# Alternative could be -1 * totalDistance
+    return 1/totalDistance
+
+
+print(fitness([1,2,3,4,5,6,7]))
